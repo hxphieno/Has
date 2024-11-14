8e3a60bcf9cdbf7c133b1209c7c524e551510b09
hxphieno <hx_phieno@tju.edu.cn>
2024-11-14 23:25:40 +0800

    ·

---
+++ 聚类/cutwindow.py
@@ -0,0 +1,59 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from sklearn.cluster import DBSCAN, KMeans
+
+with open('dataset2/power.txt', 'r') as file:
+    content = file.read()
+
+content = [i.split(",") for i in content.split("\n")]
+df = pd.DataFrame(content).apply(pd.to_numeric, errors='coerce')
+df=df[3000:10000]
+
+df = df.apply(lambda row:
+              row.fillna(df.mean(axis=1, skipna=True)), axis=1)
+
+df['mean'] = df.mean(axis=1)
+df['var'] = df.drop("mean", axis=1).var(axis=1)
+
+X = df[['mean', 'var']]
+
+if (False):
+    dbscan = DBSCAN(eps=5, min_samples=5)
+    dbscan.fit(X)
+    labels = dbscan.labels_
+    df['cluster'] = labels
+    outliers = df[df['cluster'] == -1]
+else:
+    kmeans = KMeans(n_clusters=5)
+    kmeans.fit(X)
+    labels = kmeans.labels_
+    df['cluster'] = labels
+    df['distance_to_center'] = kmeans.transform(X).min(axis=1)
+    outlier_ratio = 0.1
+    outliers_per_cluster = int(outlier_ratio * len(df) / kmeans.n_clusters)
+    outliers = df.nlargest(outliers_per_cluster, 'distance_to_center')
+
+
+
+# 打印异常值
+print("异常值：")
+print(outliers)
+
+# 根据index获取原文中的异常值
+
+data = pd.read_csv("dataset2/pv.csv")
+
+print("异常原值：")
+print(data.loc[outliers.index])
+
+plt.figure(figsize=(10, 6))
+for column in data.loc[outliers.index].columns:
+    if column != 'time':
+        plt.plot(data.loc[outliers.index]['time'], data.loc[outliers.index][column], label=column)
+
+plt.xlabel('Time')
+plt.ylabel('Value')
+plt.title('Power Data Plot for Outliers')
+plt.legend()
+plt.show()
--- 聚类/election.py
+++ 聚类/election.py
@@ -1,8 +1,99 @@
-import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.cluster import DBSCAN,KMeans
 
-df=pd.read_csv("dataset2/pv.csv")
-print(df.head())
-print(df.columns)
+# 加载数据
+df = pd.read_csv('dataset2/pv.csv', parse_dates=['time'], index_col='time')
 
-plt.figure(figsize=(12,6))
+df = df.dropna()
+
+
+
+
+
+
+
+# # DBSCAN
+# db = DBSCAN(eps=4, min_samples=2).fit(df)
+# df['cluster'] = db.labels_
+#
+# # 筛选出异常点（标签为-1的样本）
+# outliers = df[df['cluster'] == -1]
+#
+# print("异常点:")
+# print(outliers)
+#
+# # 可视化数据和异常点
+# plt.figure(figsize=(10, 6))  # 设置图表大小
+# sns.scatterplot(data=outliers, x='power', y='generation', color='red')  # 使用Seaborn绘制散点图
+# plt.title('Outliers in Power and Generation')  # 设置图表标题
+# plt.xlabel('Power')  # 设置x轴标签
+# plt.ylabel('Generation')  # 设置y轴标签
+# plt.show()
+
+#K-means
+# kmeans=KMeans(n_clusters=3)
+# # 使用 K-means 进行聚类
+# df['cluster'] = kmeans.fit_predict(df)
+#
+# centroids = kmeans.cluster_centers_
+#
+# # 计算每个样本到质心的距离
+# distances = np.linalg.norm(df[['power','generation']].values - centroids[df['cluster']], axis=1)
+#
+# # 设置一个阈值，认为离群点是距离质心较远的点
+# threshold = np.mean(distances) + 20 * np.std(distances)
+#
+# # 筛选出异常点（距离质心较远的点）
+# outliers = df[distances > threshold]
+#
+# # 输出异常点
+# print("异常点:")
+# print(outliers)
+# print(outliers.shape[0])
+#
+# # 可视化数据和异常点
+# plt.figure(figsize=(10, 6))  # 设置图表大小
+# sns.scatterplot(data=outliers, x='power', y='generation', color='red')  # 使用Seaborn绘制散点图
+# plt.title('Outliers in Power and Generation')  # 设置图表标题
+# plt.xlabel('Power')  # 设置x轴标签
+# plt.ylabel('Generation')  # 设置y轴标签
+# plt.show()
+
+# plt.figure(figsize=(7, 5))
+
+# # 趋势折线图
+# plt.plot(df.index, df['power'], label='Power')
+# plt.legend()
+# plt.title('Power Trend Line Plot')
+# plt.xlabel('Time')
+# plt.ylabel('Value')
+# plt.show()
+#
+# plt.plot(df.index, df['generation'], label='Generation')
+# plt.legend()
+# plt.title('Generation Trend Line Plot')
+# plt.xlabel('Time')
+# plt.ylabel('Value')
+# plt.show()
+#
+# # 散点图
+# plt.scatter(df.index, df['power'], alpha=0.5)
+# plt.title('Scatter Plot of Power')
+# plt.xlabel('Time')
+# plt.ylabel('Power')
+# plt.show()
+#
+# plt.scatter(df.index, df['generation'], alpha=0.5)
+# plt.title('Scatter Plot of Generation')
+# plt.xlabel('Time')
+# plt.ylabel('Generation')
+# plt.show()
+#
+# # 盒图
+# plt.figure(figsize=(10, 5))
+# sns.boxplot(data=df[['power', 'generation']])
+# plt.title('Boxplot of Power and Generation')
+# plt.show()
